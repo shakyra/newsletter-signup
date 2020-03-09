c89219b40c7b4da1ada18a03ba4a89315145ed42
Shakyra Clark <shakyraclark@sac-mbp.local>
2020-03-09 12:06:23 -0400

    Changed port

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.post("/failure", function(req, res) {
   res.redirect("/"); // redirect failure button to sign up page
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT, function() {
   console.log("Listening on port 3000!")
 });
 
